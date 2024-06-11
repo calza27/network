ecfbdb115c0de488c6892dfc2d6ce7cba7026486
calza27 <calza27@gmail.com>
2024-06-11 16:55:43 +1000

    Complete deploy script and yaml parser

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
 die() { echo ${1:-argh} exit ${2:-1}; }
 
+hash aws || die "aws not found."
+hash ./bin/parse-yaml.sh || die "parse-yaml.sh not found."
+
+profile=$1
+[[ -z $profile ]] && die "Usage: $0 <profile>"
+
+STACK_NAME="network"
+params=$(./bin/parse-yaml.sh ./params/params.yml)
+tags=$(./bin/parse-yaml.sh ./params/tags.yml)
+
 aws cloudformation deploy /
---capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND /
---stack-name network /
---template-file cf/net.yml /
---parameter-overrides $(cat params/dev.yml) /
+    --capabilities "CAPABILITY_NAMED_IAM" "CAPABILITY_AUTO_EXPAND" /
+    --s3-bucket "cfn-templates-390315154367" /
+    --s3-prefix "$STACK_NAME"
+    --stack-name "$STACK_NAME" /
+    --template-file "./cf/net.yml" /
+    --parameter-overrides "$params" /
+    --tags "$tags" /
+    --profile "$profile" || die "failed to deploy stack "$STACK_NAME""
