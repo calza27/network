3541b68df25735d0c90629e7879f51f1fd960f4a
calza27 <calza27@gmail.com>
2024-06-11 16:57:28 +1000

    Fix deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -11,12 +11,12 @@ STACK_NAME="network"
 params=$(./bin/parse-yaml.sh ./params/params.yml)
 tags=$(./bin/parse-yaml.sh ./params/tags.yml)
 
-aws cloudformation deploy /
-    --capabilities "CAPABILITY_NAMED_IAM" "CAPABILITY_AUTO_EXPAND" /
-    --s3-bucket "cfn-templates-390315154367" /
-    --s3-prefix "$STACK_NAME"
-    --stack-name "$STACK_NAME" /
-    --template-file "./cf/net.yml" /
-    --parameter-overrides "$params" /
-    --tags "$tags" /
+aws cloudformation deploy \
+    --capabilities "CAPABILITY_NAMED_IAM" "CAPABILITY_AUTO_EXPAND" \
+    --s3-bucket "cfn-templates-390315154367" \
+    --s3-prefix "$STACK_NAME" \
+    --stack-name "$STACK_NAME" \
+    --template-file "./cf/net.yml" \
+    --parameter-overrides "$params" \
+    --tags "$tags" \
     --profile "$profile" || die "failed to deploy stack "$STACK_NAME""
