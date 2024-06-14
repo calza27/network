b94df781f83769b2da63ba37cca3618df6c39594
calza27 <calza27@gmail.com>
2024-06-14 22:09:31 +1000

    Fix up deploy script and add vpc endpoints

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -10,13 +10,14 @@ profile=$1
 STACK_NAME="network"
 params=$(./bin/parse-yaml.sh ./params/params.yml) || die "failed to parse params"
 tags=$(./bin/parse-yaml.sh ./params/tags.yml) || die "failed to parse tags"
+bucket_name=$(aws ssm get-parameter --profile "$profile" --name /s3/cfn-bucket/name --query "Parameter.Value" --output text) || die "failed to get name of cfn bucket"
 
 aws cloudformation deploy \
     --capabilities "CAPABILITY_NAMED_IAM" "CAPABILITY_AUTO_EXPAND" \
-    --s3-bucket "cfn-templates-3903---4367" \
+    --s3-bucket "${bucket_name}" \
     --s3-prefix "$STACK_NAME" \
     --stack-name "$STACK_NAME" \
     --template-file "./cf/net.yml" \
     --parameter-overrides "[$params]" \
     --tags "[$tags]" \
-    --profile "$profile" || die "failed to deploy stack "$STACK_NAME""
+    --profile "${profile}" || die "failed to deploy stack "$STACK_NAME""
